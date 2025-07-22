63fb89ab3a176089cad592aa38fde14a3f7150fe
Eric Molitor <534583+emolitor@users.noreply.github.com>
2025-07-22 15:30:11 +0100

    fix: Remove useless guarding on battery_custom

--- keyboards/shortcut/wireless/battery_custom.c
+++ keyboards/shortcut/wireless/battery_custom.c
@@ -2,11 +2,9 @@
 #include <stdint.h>
 #include "module.h"
 
-#ifdef BATTERY_DRIVER
 void battery_driver_init(void) {
 }
 
 uint8_t battery_driver_sample_percent(void) {
     return *md_getp_bat();
 }
-#endif
