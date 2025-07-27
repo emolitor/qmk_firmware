80d4be35d8cb75c5ff23f23c7affdde4ed78a0c8
Eric Molitor <534583+emolitor@users.noreply.github.com>
2025-07-27 12:27:52 +0100

    fix: Remove lpwr_wb32

--- keyboards/shortcut/bridge75/rules.mk
+++ keyboards/shortcut/bridge75/rules.mk
@@ -1,7 +1,1 @@
-BATTERY_DRIVER_REQUIRED = yes
-BATTERY_DRIVER = custom
-
-BLUETOOTH_ENABLE = yes
-BLUETOOTH_DRIVER = custom
-
 include keyboards/shortcut/wireless/wireless.mk
--- keyboards/shortcut/wireless/wireless.mk
+++ keyboards/shortcut/wireless/wireless.mk
@@ -3,6 +3,10 @@ WIRELESS_DIR = $(TOP_DIR)/keyboards/shortcut/wireless
 
 ifeq ($(strip $(WIRELESS_ENABLE)), yes)
     OPT_DEFS += -DWIRELESS_ENABLE -DNO_USB_STARTUP_CHECK
+	BATTERY_DRIVER_REQUIRED = yes
+	BATTERY_DRIVER = custom
+	BLUETOOTH_ENABLE = yes
+	BLUETOOTH_DRIVER = custom
 
     UART_DRIVER_REQUIRED ?= yes
     WIRELESS_LPWR_STOP_ENABLE ?= yes
@@ -14,9 +18,4 @@ ifeq ($(strip $(WIRELESS_ENABLE)), yes)
         $(WIRELESS_DIR)/lowpower.c \
         $(WIRELESS_DIR)/smsg.c \
         $(WIRELESS_DIR)/module.c
-
-    ifeq ($(strip $(WIRELESS_LPWR_STOP_ENABLE)), yes)
-        OPT_DEFS += -DWIRELESS_LPWR_STOP_ENABLE
-        SRC += $(WIRELESS_DIR)/lpwr_wb32.c
-    endif
 endif
