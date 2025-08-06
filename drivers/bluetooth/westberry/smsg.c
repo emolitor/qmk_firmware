1844185f366599061cb97ed5e24feb8f20d1761e
Eric Molitor <534583+emolitor@users.noreply.github.com>
2025-08-06 15:28:25 +0100

    fix: Merge linting fixes from rfc branch

--- drivers/bluetooth/westberry/smsg.c
+++ drivers/bluetooth/westberry/smsg.c
@@ -1,3 +1,4 @@
+// Copyright 2025 emolitor (github.com/emolitor)
 // Copyright 2024 Su (@isuua)
 // SPDX-License-Identifier: GPL-2.0-or-later
 
@@ -10,9 +11,9 @@
 #endif
 
 static smsg_message_t smsg_buffer[SMSG_NUM] __attribute__((aligned(PORT_NATURAL_ALIGN)));
-static msg_t smsg_mailbox_buffer[SMSG_NUM];
+static msg_t          smsg_mailbox_buffer[SMSG_NUM];
 static objects_fifo_t smsg_fifo;
-static smsg_states_t smsg_state;
+static smsg_states_t  smsg_state;
 
 void smsg_init(void) {
     chFifoObjectInit(&smsg_fifo, sizeof(smsg_message_t), SMSG_NUM, smsg_buffer, smsg_mailbox_buffer);
@@ -20,13 +21,12 @@ void smsg_init(void) {
 }
 
 smsg_message_t *smsg_take(void) {
-    // Acquire a message to the pool
+    // Acquire a message from the memory pool
     return (smsg_message_t *)chFifoTakeObjectTimeout(&smsg_fifo, TIME_IMMEDIATE);
 }
 
-
 void smsg_return(smsg_message_t *msg) {
-    // Return the current message to the pool
+    // Return the message to the memory pool
     if (msg != NULL) {
         chFifoReturnObject(&smsg_fifo, msg);
     }
@@ -37,8 +37,8 @@ void smsg_send(smsg_message_t *msg) {
 }
 
 smsg_message_t *smsg_receive(void) {
-    smsg_message_t *msg = NULL;
-    msg_t result = chFifoReceiveObjectTimeout(&smsg_fifo, (void **)&msg, TIME_IMMEDIATE);
+    smsg_message_t *msg    = NULL;
+    msg_t           result = chFifoReceiveObjectTimeout(&smsg_fifo, (void **)&msg, TIME_IMMEDIATE);
     if (result != MSG_OK) {
         return NULL;
     }
@@ -54,6 +54,6 @@ void smsg_set_state(smsg_states_t state) {
 }
 
 bool smsg_is_busy(void) {
-    // Check if there are messages in the mailbox
+    // Check if there are any pending messages in the FIFOs mailbox
     return (chMBGetUsedCountI(&smsg_fifo.mbx) > 0);
 }
