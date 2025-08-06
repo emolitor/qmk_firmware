2aa00b3b41b3730f4768ecb6e9cea6c653293fa3
Eric Molitor <534583+emolitor@users.noreply.github.com>
2025-08-06 11:57:53 +0100

    feat: Use ChibiOS Object Fifo

--- drivers/bluetooth/westberry/smsg.h
+++ drivers/bluetooth/westberry/smsg.h
@@ -6,6 +6,15 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+#ifndef SMSG_PAYLOAD_LEN
+#    define SMSG_PAYLOAD_LEN 50
+#endif
+
+typedef struct {
+    uint8_t data[SMSG_PAYLOAD_LEN];
+    uint32_t size;
+} smsg_message_t;
+
 typedef enum { smsg_state_free = 0, smsg_state_busy, smsg_state_retry, smsg_state_replied } smsg_states_t;
 
 void          smsg_init(void);
