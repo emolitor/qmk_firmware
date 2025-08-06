19ad8f1f88a0b1fea2a4eac778e72929fd58e380
Eric Molitor <534583+emolitor@users.noreply.github.com>
2025-08-06 13:28:30 +0100

    feat: Remove old smsg code

--- drivers/bluetooth/westberry/smsg.c
+++ drivers/bluetooth/westberry/smsg.c
@@ -12,7 +12,6 @@
 typedef struct {
     smsg_states_t state;
     objects_fifo_t fifo;
-    smsg_message_t *current_msg;  // Store the current peeked message
 } smsg_t;
 
 static smsg_message_t smsg_buffer[SMSG_NUM] __attribute__((aligned(PORT_NATURAL_ALIGN)));
@@ -22,7 +21,6 @@ static smsg_t smsg_instance;
 void smsg_init(void) {
     chFifoObjectInit(&smsg_instance.fifo, sizeof(smsg_message_t), SMSG_NUM, smsg_buffer, smsg_mailbox_buffer);
     smsg_instance.state = smsg_state_free;
-    smsg_instance.current_msg = NULL;
 }
 
 smsg_message_t *smsg_take(void) {
@@ -51,42 +49,6 @@ smsg_message_t *smsg_receive(void) {
     return msg;
 }
 
-bool smsg_push(uint8_t *buf, uint32_t size) {
-    if (size > SMSG_PAYLOAD_LEN) {
-        return false;
-    }
-
-    smsg_message_t *msg = smsg_take();
-    if (msg == NULL) {
-        return false;
-    }
-
-    memcpy(msg->data, buf, size);
-    msg->size = size;
-
-    smsg_send(msg);
-    return true;
-}
-
-uint32_t smsg_peek(uint8_t *buf) {
-    // If we don't have a current message, try to receive one
-    if (smsg_instance.current_msg == NULL) {
-        smsg_instance.current_msg = smsg_receive();
-        if (smsg_instance.current_msg == NULL) {
-            return 0;
-        }
-    }
-
-    // Copy the data from the current message
-    memcpy(buf, smsg_instance.current_msg->data, smsg_instance.current_msg->size);
-    return smsg_instance.current_msg->size;
-}
-
-void smsg_pop(void) {
-    smsg_return(smsg_instance.current_msg);
-    smsg_instance.current_msg = NULL;
-}
-
 smsg_states_t smsg_get_state(void) {
     return smsg_instance.state;
 }
@@ -96,6 +58,6 @@ void smsg_set_state(smsg_states_t state) {
 }
 
 bool smsg_is_busy(void) {
-    // Check if we have a current message or if there are messages in the mailbox
-    return (smsg_instance.current_msg != NULL) || (chMBGetUsedCountI(&smsg_instance.fifo.mbx) > 0);
+    // Check if there are messages in the mailbox
+    return (chMBGetUsedCountI(&smsg_instance.fifo.mbx) > 0);
 }
