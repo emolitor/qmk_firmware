83e4bfe1d384bba53a1c6b1b3a9dadeecc89fb56
Eric Molitor <534583+emolitor@users.noreply.github.com>
2025-08-12 16:13:25 +0100

    feat: Move systick to TIM3 ahead of testing PWM driver

--- keyboards/shortcut/bridge75/mcuconf.h
+++ keyboards/shortcut/bridge75/mcuconf.h
@@ -11,3 +11,16 @@
 
 #undef WB32_SPI_USE_QSPI
 #define WB32_SPI_USE_QSPI TRUE
+
+// The SysTick timer from the normal quantum/WB32 uses TIM2 -- the WS2812 pin used
+// on the Bridge75 requires the use of TIM2 to run PWM -- rework which timers are
+// allocated for PWM usage.
+#undef WB32_PWM_USE_TIM2
+#undef WB32_PWM_USE_TIM3
+#define WB32_PWM_USE_TIM2 TRUE
+#define WB32_PWM_USE_TIM3 FALSE
+
+// As mentioned above, we need to reallocate the SysTick timer used from
+// TIM2 to TIM3.
+#undef WB32_ST_USE_TIMER
+#define WB32_ST_USE_TIMER 3
